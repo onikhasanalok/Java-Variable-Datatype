da03315ad684db54b9b444af0d5102ab8dcfeb2b
Onik Hasan <onikhasanalok123@gmail.com>
2024-07-08 19:29:19 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Java Variable of intiger and Float/Double
+        System.out.println("Here Variable Part:");
+        int a = 20;
+        double b = 4.25;
+        float c = 3.44445f;
+        double d = a+b+c;
+
+        System.out.println("Result of a+b+c = "+d+"\n");
+
+        //Java Datatype of Boolean
+        System.out.println("Here Datatype of Boolean:");
+        boolean a1 = true;
+        boolean b1 = false;
+        System.out.println("The Result of a1= "+a1);
+        System.out.println("The Result of b1= "+b1+"\n");
+
+        //Java Datatype of Byte
+        System.out.println("Here Datatype of Byte:");
+        byte a2= 100;
+        byte b2=-100;
+        System.out.println("The result of byte a2: "+a2);
+        System.out.println("The result of byte b2: "+b2+"\n");
+
+        //Java Datatype of short
+        System.out.println("Here Datatype of Short:");
+        short a3=10000;
+        short b3= -10000;
+        System.out.println("The result of short a3: "+a3);
+        System.out.println("The result of short b3: "+b3+"\n");
+
+        //Java Datatype of integer
+        System.out.println("Here Datatype of Integer:");
+        int a4=10;
+        int b4= -120;
+        System.out.println("The result of integer a4: "+a4);
+        System.out.println("The result of integer b4: "+b4+"\n");
+
+        //Java Datatype of double
+        System.out.println("Here Datatype of Double:");
+        double a5=10.33;
+        double b5= 120.77;
+        System.out.println("The result of double a5: "+a5);
+        System.out.println("The result of double b5: "+b5+"\n");
+
+
+        //Java Datatype of float
+        System.out.println("Here Datatype of Float:");
+        float a6=10.33f;
+        float b6= 120.77f;
+        System.out.println("The result of float a6: "+a6);
+        System.out.println("The result of float b6: "+b6+"\n");
+
+        //Java Datatype of character
+        System.out.println("Here Datatype of Character:");
+        char a7='B';
+        char b7= 'C';
+        System.out.println("The result of character a7: "+a7);
+        System.out.println("The result of character b7: "+b7+"\n");
+
+        //Java Datatype of Strings
+        System.out.println("Here Datatype of Strings:");
+        String a8="Daily Star";
+        String b8= "of Bangladesh";
+        System.out.println("The result of Strings a8: "+a8);
+        System.out.println("The result of Strings b8: "+b8+"\n");
+
+        //Java Datatype of Long
+        System.out.println("Here Datatype of Long:");
+        long a9=1000000L;
+        long b9= 500000000L;
+        System.out.println("The result of Long a9: "+a9);
+        System.out.println("The result of Long b9: "+b9+"\n");
+
+    }
+
+}
